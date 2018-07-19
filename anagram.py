f9726d8b7a2504478bd5c638632b2f1a1f41166c
vhuntsman <vhuntsman@proton.me>
2018-07-20 00:18:41 +0800

    Add main anagram code. 30 seconds.

---
+++ anagram.py
@@ -0,0 +1,69 @@
+import string
+
+def buildHashTable(handle, hashtable):
+    values = dict()
+    for index, letter in enumerate(string.ascii_lowercase):
+        values[letter] = index + 1
+    for line in handle:
+        hashval = 0
+        for char in line.strip().lower():
+            hashval+=values[char]
+        key = str(hashval)
+        if key in hashtable:
+            hashtable[key].append(line.strip())
+        else:
+            hashtable[key] = [line.strip()]
+
+def hashKey(strDut):
+    values = dict()
+    for index, letter in enumerate(string.ascii_lowercase):
+        values[letter] = index + 1
+    hashval = 0
+    for char in strDut.lower():
+        hashval += values[char]
+    return str(hashval)
+
+def permutate(stringData):
+    listdata = list(stringData)
+    found = False
+    global anastack
+    global anagram
+    for i, char in enumerate(listdata):
+        if len(listdata) == 1:
+            anagram = ''.join(anastack) + char
+            anagrams.append(anagram)
+        else:
+            templist = list(listdata)
+            del templist[i]
+            anastack.append(char)
+            result = permutate(''.join(templist))
+        if i == (len(listdata) - 1):
+            if (len(anastack) != 0):
+                anastack.pop()
+
+fDict = open("9dict.txt", "r")
+hashtable = dict()
+buildHashTable(fDict, hashtable)
+
+anagrams = []
+anagram = ""
+anastack = []
+
+DUT = "AGOSMRONM"
+
+dutHash = hashKey(DUT)
+listValues = hashtable[dutHash]
+
+permutate(DUT)
+
+foundGram = []
+
+for anagram in anagrams:
+    if anagram.lower() in listValues:
+        if anagram not in foundGram:
+            foundGram.append(anagram)
+
+for found in foundGram:
+    print found
+
+fDict.close()
